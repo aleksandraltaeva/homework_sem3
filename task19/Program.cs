e2a3d4703185ad3d083eed3f61dbddaec2158a6e
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-19 18:20:35 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return Console.ReadLine()!;
+}
+
+void Pallindrome(string num)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"{num} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> нет");
+    }
+}
+
+string number = ReadInt("Введите пятизначное число: ");
+if (number.Length == 5)
+{
+   Pallindrome(number);
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число.");
+}
